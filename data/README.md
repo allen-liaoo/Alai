178336a0c83086db0a4b2e5a0455436e9e0f3124
Allen Liao <wcliaw610@gmail.com>
2024-12-26 14:11:17 -0600

    process ios messages

---
+++ data/README.md
@@ -0,0 +1,9 @@
+Each data source contains `/raw` and `/processed` directories. `/processed` directory should contain `.txt` files of the form:
+```
+##[Sent| Recieved]
+[Message]
+
+##[Sent| Recieved]
+[Message]
+```
+With new line inbetween each message.
---
+++ data/ios_messages/process_raw_data.py
@@ -0,0 +1,37 @@
+# Used https://github.com/ReagentX/imessage-exporter to extract ios messages
+# imessage-exporter -f html -c disabled -a iOS -o raw
+
+import os
+import pathlib
+from lxml import html as HTMLParser
+
+cwd_path = pathlib.Path(__file__).parent
+raw_data_path = cwd_path / 'raw'
+data_path = cwd_path / 'processed'
+
+for filename in os.listdir(raw_data_path):
+    if not filename.endswith('.html'): continue
+    with open(raw_data_path / filename, 'r') as file:
+        try:
+            html = HTMLParser.parse(file)
+        except:
+            print(f'error reading file {filename}, ignored')
+            continue
+
+    elements = html.xpath("//body/div[contains(@class, 'message')] \
+                          /div[contains(@class, 'sent') or contains(@class, 'received')]")
+    msgs = []
+
+    for e in elements:
+        msg_content = e.xpath("./div[contains(@class, 'message_part')]/span")
+        if not msg_content or len(msg_content) == 0: continue
+        msg = msg_content[0].text_content()
+
+        sender = e.xpath("./p/span[contains(@class, 'sender')]/text()")
+        if not sender or len(sender) == 0: continue
+        category = 'Sent' if sender[0] == "Me" else 'Recieved'
+
+        msgs.append(f'##{category}\n{msg}\n\n')
+
+    with open(data_path / filename.replace('.html', '.txt'), 'w') as file:
+        file.write(''.join(msgs))
